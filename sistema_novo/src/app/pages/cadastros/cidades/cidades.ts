d23a2e12103d122b68e7838e5261e44dfb9a28e1
ricardops34 <ricardopataysotomayor@gmail.com>
2026-03-24 15:04:49 -0400

    feat: implementação dos CRUDs dinâmicos de Estados e Cidades

--- sistema_novo/src/app/pages/cadastros/cidades/cidades.ts
+++ sistema_novo/src/app/pages/cadastros/cidades/cidades.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { PoPageDynamicTableModule } from '@po-ui/ng-templates';
+import { PoBreadcrumb, PoPageDynamicTableField } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-cidades',
-  imports: [],
+  standalone: true,
+  imports: [PoPageDynamicTableModule],
   templateUrl: './cidades.html',
-  styleUrl: './cidades.css',
 })
-export class Cidades {}
+export class Cidades {
+  readonly breadcrumb: PoBreadcrumb = {
+    items: [{ label: 'Home', link: '/' }, { label: 'Cadastros' }, { label: 'Cidades' }],
+  };
+
+  readonly fields: Array<PoPageDynamicTableField> = [
+    { property: 'id', key: true, label: 'Id', filter: true, width: '10%' },
+    { property: 'estadoNome', label: 'Estado', filter: true },
+    { property: 'nome', label: 'Nome', filter: true },
+  ];
+}
--- sistema_novo/src/app/pages/cadastros/estados/estados.ts
+++ sistema_novo/src/app/pages/cadastros/estados/estados.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { PoPageDynamicTableModule } from '@po-ui/ng-templates';
+import { PoBreadcrumb, PoPageDynamicTableField } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-estados',
-  imports: [],
+  standalone: true,
+  imports: [PoPageDynamicTableModule],
   templateUrl: './estados.html',
-  styleUrl: './estados.css',
 })
-export class Estados {}
+export class Estados {
+  readonly breadcrumb: PoBreadcrumb = {
+    items: [{ label: 'Home', link: '/' }, { label: 'Cadastros' }, { label: 'Estados' }],
+  };
+
+  readonly fields: Array<PoPageDynamicTableField> = [
+    { property: 'id', key: true, label: 'Id', filter: true, width: '10%' },
+    { property: 'nome', label: 'Nome', filter: true },
+  ];
+}
